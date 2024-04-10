80d360d5c6b12e938629967a340820e16e5c5021
IIGeekingII <aldo.bocti@gmail.com>
2024-04-10 18:00:06 +0200

    Sounds and Particles
    + child class PlayerHealth : Mainhealth
    Add explosion feature (particlesys +audio)
    Add aufio fx too projectiles (player and enemy)

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MainHealth
+{
+    public ParticleSystem explosion;
+
+    protected override void Death()
+    {
+        if (health <= 0)
+        {
+            Instantiate(explosion, gameObject.transform.position, gameObject.transform.localRotation);
+            Destroy(gameObject);
+        }
+    }
+}
